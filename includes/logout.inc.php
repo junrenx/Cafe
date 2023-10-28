5fca15bde0fc4be50982eb4dd5e52cfaa8fca6ec
junrenx <147378306+junrenx@users.noreply.github.com>
2023-10-28 14:57:22 +0800

    Delete includes directory

--- includes/logout.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-  // Logout user by destroying session
-  session_start();
-  session_unset();
-  session_destroy();
-
-  // Return user back to log in page
-  header('Location: ../index.php');
